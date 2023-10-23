c77977c523a1f3b6349f8b332d5893c853af0c3e
Sean Shubin <sshubin@cj.com>
2023-10-22 19:39:13 -0700

    better error messaging

--- domain/src/main/kotlin/com/seanshubin/code/structure/domain/ErrorHandlerImpl.kt
+++ domain/src/main/kotlin/com/seanshubin/code/structure/domain/ErrorHandlerImpl.kt
@@ -1,7 +1,6 @@
 package com.seanshubin.code.structure.domain
 
-import com.seanshubin.code.structure.collection.ComparatorUtil.pairComparator
-import com.seanshubin.code.structure.collection.ComparatorUtil.stringComparator
+import com.seanshubin.code.structure.collection.ComparatorUtil
 import com.seanshubin.code.structure.collection.SetUtil
 import com.seanshubin.code.structure.contract.delegate.FilesContract
 import com.seanshubin.code.structure.domain.ErrorsDto.Companion.toDto
@@ -30,74 +29,101 @@ class ErrorHandlerImpl(
     }
 
     private fun compareErrors(old: Errors, current: Errors, failConditions: FailConditions): Int {
-        val directCycles = compareReport(
-            "Direct Cycles",
-            failConditions.directCycle,
-            old.inDirectCycle,
-            current.inDirectCycle,
-            stringComparator,
-            stringFormat
-        )
-        val groupCycles = compareReport(
-            "Group Cycles",
-            failConditions.groupCycle,
-            old.inGroupCycle,
-            current.inGroupCycle,
-            stringComparator,
-            stringFormat
-        )
-        val ancestorDependsOnDescendant = compareReport(
-            "Ancestor depends on Descendant",
-            failConditions.ancestorDependsOnDescendant,
-            old.ancestorDependsOnDescendant,
-            current.ancestorDependsOnDescendant,
-            pairComparator,
-            referenceFormat
-        )
-        val descendantDependsOnAncestor = compareReport(
-            "Descendant depends on Ancestor",
-            failConditions.descendantDependsOnAncestor,
-            old.descendantDependsOnAncestor,
-            current.descendantDependsOnAncestor,
-            pairComparator,
-            referenceFormat
-        )
-        val lines = directCycles.lines+
-                groupCycles.lines +
-                ancestorDependsOnDescendant.lines +
-                descendantDependsOnAncestor.lines
+        val errorInfo = ErrorInfo(old, current, failConditions)
+        val errorReports = ErrorDimension.values().map { it.analyze(errorInfo) }
+        val anyFailed = errorReports.any{it.isFail}
+        val exitCode = if(anyFailed) 1 else 0
+        val lines = errorReports.flatMap{it.lines}
         errorReportEvent(lines)
-        val exitCode = if(directCycles.fail || groupCycles.fail || ancestorDependsOnDescendant.fail || descendantDependsOnAncestor.fail){
-            1
-        } else {
-            0
-        }
         return exitCode
     }
 
-    private fun <T> compareReport(
-        caption: String,
-        failPossible: Boolean,
-        old: List<T>,
-        current: List<T>,
-        comparator: Comparator<T>,
-        format: (T) -> String
-    ): CompareReport {
-        val compare = SetUtil.compare(old.toSet(), current.toSet())
-        if (compare.isSame()) return CompareReport(fail = false, emptyList())
-        val addedList = compare.extra.toList().sortedWith(comparator).map(format)
-        val removedList = compare.missing.toList().sortedWith(comparator).map(format)
-        val lines = listOf(caption) + captionList("added", addedList) + captionList("removed", removedList)
-        val fail = failPossible && addedList.isNotEmpty()
-        return CompareReport(fail, lines)
+    data class ErrorReport(val isFail: Boolean, val lines: List<String>)
+
+    class ErrorInfo(val old: Errors, val current: Errors, val failConditions: FailConditions)
+
+    interface ErrorElement:Comparable<ErrorElement>{
+        fun formatted():String
+    }
+    data class StringErrorElement(val value: String) : ErrorElement {
+        override fun compareTo(other: ErrorElement): Int {
+            other as StringErrorElement
+            return ComparatorUtil.stringComparator.compare(this.value, other.value)
+        }
+
+        override fun formatted(): String = value
+    }
+    data class PairErrorElement(val value: Pair<String, String>) : ErrorElement {
+        override fun compareTo(other: ErrorElement): Int {
+            other as PairErrorElement
+            return ComparatorUtil.pairComparator.compare(this.value, other.value)
+        }
+
+        override fun formatted(): String {
+            val (first, second) = value
+            return "$first -> $second"
+
+        }
     }
 
-    private fun captionList(caption: String, list: List<String>): List<String> =
-        if (list.isEmpty()) emptyList()
-        else listOf("  $caption") + list.map { "    $it" }
 
-    private val stringFormat: (String) -> String = { it }
-    private val referenceFormat: (Pair<String, String>) -> String = { (first, second) -> "$first -> $second" }
+    enum class ErrorDimension(val caption: String) {
+        DIRECT_CYCLE("Direct Cycle") {
+            override fun fetchElements(errors: Errors): Set<ErrorElement> {
+                return errors.inDirectCycle.map { StringErrorElement(it) }.toSet()
+            }
 
-    private data class CompareReport(val fail: Boolean, val lines: List<String>)
+            override fun fetchCanFail(failConditions: FailConditions): Boolean {
+                return failConditions.directCycle
+            }
+        },
+        GROUP_CYCLE("Group Cycle"){
+            override fun fetchElements(errors: Errors): Set<ErrorElement> {
+                return errors.inGroupCycle.map{ StringErrorElement(it)}.toSet()
+            }
+
+            override fun fetchCanFail(failConditions: FailConditions): Boolean {
+                return failConditions.groupCycle
+            }
+        },
+        ANCESTOR_DEPENDS_ON_DESCENDANT("Ancestor Depends On Descendant"){
+            override fun fetchElements(errors: Errors): Set<ErrorElement> {
+                return errors.ancestorDependsOnDescendant.map { PairErrorElement(it)}.toSet()
+            }
+
+            override fun fetchCanFail(failConditions: FailConditions): Boolean {
+                return failConditions.ancestorDependsOnDescendant
+            }
+        },
+        DESCENDANT_DEPENDS_ON_ANCESTOR("Descendant Depends On Ancestor"){
+            override fun fetchElements(errors: Errors): Set<ErrorElement> {
+                return errors.descendantDependsOnAncestor.map { PairErrorElement(it)}.toSet()
+            }
+
+            override fun fetchCanFail(failConditions: FailConditions): Boolean {
+                return failConditions.descendantDependsOnAncestor
+            }
+        };
+
+        fun analyze(errorInfo: ErrorInfo): ErrorReport {
+            val old = fetchElements(errorInfo.old)
+            val current = fetchElements(errorInfo.current)
+            val compareResult = SetUtil.compare(old, current)
+            val canFail = fetchCanFail(errorInfo.failConditions)
+            val newErrors = compareResult.extra.toList().sorted().distinct()
+            val hasNewErrors = newErrors.isNotEmpty()
+            val isFail = canFail && hasNewErrors
+            val lines: List<String> = if (hasNewErrors) {
+                val newFailuresHeader = "ERROR New failures in $caption: ${newErrors.size}"
+                val newFailuresLines = newErrors.map { "  ${it.formatted()}" }
+                listOf(newFailuresHeader) + newFailuresLines
+            } else {
+                emptyList()
+            }
+            return ErrorReport(isFail, lines)
+        }
+
+        abstract fun fetchElements(errors: Errors): Set<ErrorElement>
+        abstract fun fetchCanFail(failConditions: FailConditions): Boolean
+    }
 }
