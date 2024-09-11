86fd78e96b1bb8a9b187a9b974444a6385aa0e31
Sean Shubin <sshubin@cj.com>
2024-09-10 17:32:52 -0700

    added script

---
+++ scripts/package-stress.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+date
+time \
+./scripts/_build_skip_tests.sh && \
+./scripts/_stress.sh "$@"
+date
+
+open -a "Google Chrome" "generated/stress-test-project/depth-$1-breadth-$2/generated/index.html"
+say "Done with stress test of depth $1 and breadth $2"
