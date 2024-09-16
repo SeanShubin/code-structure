408b1fe6314647310ec4f694220b88bef14985d0
Sean Shubin <sshubin@cj.com>
2024-09-16 09:10:38 -0700

    Added sorting utilities

--- collection/src/main/kotlin/com/seanshubin/code/structure/collection/ListUtil.kt
+++ collection/src/main/kotlin/com/seanshubin/code/structure/collection/ListUtil.kt
@@ -1,6 +1,17 @@
 package com.seanshubin.code.structure.collection
 
 object ListUtil {
+    val listComparator: Comparator<List<String>> = Comparator { o1, o2 ->
+        val sizeCompare = o1.size.compareTo(o2.size)
+        if (sizeCompare != 0) return@Comparator sizeCompare
+        val size = o1.size
+        for (i in 0 until size) {
+            val comparison = o1[i].compareTo(o2[i])
+            if (comparison != 0) return@Comparator comparison
+        }
+        0
+    }
+
     fun <T> List<T>.startsWith(prefix: List<T>): Boolean {
         if (prefix.size > size) return false
         return take(prefix.size) == prefix
---
+++ domain/src/main/kotlin/com/seanshubin/code/structure/domain/ReportSorting.kt
@@ -0,0 +1,14 @@
+package com.seanshubin.code.structure.domain
+
+import com.seanshubin.code.structure.collection.ComparatorUtil.comparingFirst
+import com.seanshubin.code.structure.collection.ComparatorUtil.pairComparator
+import com.seanshubin.code.structure.collection.ListUtil.listComparator
+
+object ReportSorting {
+    fun Set<String>.sortedForReport(): List<String> = sorted()
+    fun Set<Pair<String, String>>.sortedPairsForReport(): List<Pair<String, String>> = sortedWith(pairComparator)
+    fun Set<Set<String>>.sortedSetOfSetOfStringForReport():List<List<String>> =
+        map{it.sortedForReport()}.sortedWith(listComparator)
+    fun Map<List<String>, ScopedAnalysis>.sortedForReport():List<Pair<List<String>, ScopedAnalysis>> =
+        toList().sortedWith(comparingFirst(listComparator))
+}
