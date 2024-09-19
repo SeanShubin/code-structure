9fa53c2033bdfac0ae9dd1c522f67dbba3ad77c8
Sean Shubin <sshubin@cj.com>
2024-09-18 19:27:05 -0700

    increased memory for stress test

--- scripts/_stress.sh
+++ scripts/_stress.sh
@@ -9,5 +9,5 @@ rm -rf "$BASE_DIR"
 java -jar stress/target/code-structure-stress.jar depth "$1" breadth "$2"
 pushd "$BASE_DIR" || exit
 mvn package
-java -jar ../../../console/target/code-structure-console.jar
+java -Xmx16g -jar ../../../console/target/code-structure-console.jar
 popd || exit
