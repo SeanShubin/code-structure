97eb0e1c9b6afe531a348fd20fa82a2fa9fe6ad5
Sean Shubin <sshubin@cj.com>
2024-10-01 16:02:22 -0700

    better error handling for exec

--- exec/src/main/kotlin/com/seanshubin/code/structure/exec/ExecImpl.kt
+++ exec/src/main/kotlin/com/seanshubin/code/structure/exec/ExecImpl.kt
@@ -1,14 +1,21 @@
 package com.seanshubin.code.structure.exec
 
+import java.io.IOException
+
 class ExecImpl : Exec {
     override fun exec(vararg args: String) {
         val processBuilder = ProcessBuilder()
         processBuilder.command(*args)
-        val process = processBuilder.start()
-        val exitCode = process.waitFor()
-        if (exitCode != 0) {
+        try {
+            val process = processBuilder.start()
+            val exitCode = process.waitFor()
+            if (exitCode != 0) {
+                val command = args.joinToString(" ")
+                throw RuntimeException("Unable to execute command $command, exited with code $exitCode")
+            }
+        } catch (ex:IOException) {
             val command = args.joinToString(" ")
-            throw RuntimeException("Unable to execute command $command, exited with code $exitCode")
+            throw RuntimeException("Unable to execute command $command, exited by throwing exception", ex)
         }
     }
 }
