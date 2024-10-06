3cce12bde4a51ce2bdb935e8e49194ba5fe276ba
Sean Shubin <sshubin@cj.com>
2024-10-05 20:20:14 -0700

    removed dead code

--- collection/src/main/kotlin/com/seanshubin/code/structure/collection/SetCompareResult.kt
+++
@@ -1,9 +0,0 @@
-package com.seanshubin.code.structure.collection
-
-data class SetCompareResult<T>(
-    val missing: Set<T>,
-    val extra: Set<T>,
-    val same: Set<T>
-) {
-    fun isSame(): Boolean = missing.isEmpty() && extra.isEmpty()
-}
