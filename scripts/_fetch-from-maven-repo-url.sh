cf929a06148befe82f8a5645948b2ae4d571cd7d
Sean Shubin <sshubin@cj.com>
2025-07-14 12:29:57 -0700

    changed version from 1.0.2 to 1.0.3

--- scripts/_fetch-from-maven-repo-url.sh
+++ scripts/_fetch-from-maven-repo-url.sh
@@ -7,4 +7,4 @@ fi
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
     -DrepoUrl="$MAVEN_SNAPSHOT_URL" \
-    -Dartifact=com.seanshubin.code.structure:code-structure-console:1.0.2
+    -Dartifact=com.seanshubin.code.structure:code-structure-console:1.0.3
