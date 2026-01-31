f9bc556081ca916df8299fc14a7fb6c2e9e1682c
Sean Shubin <sshubin@cj.com>
2026-01-31 11:35:54 -0800

    renamed samples to testdata

--- scripts/elixir-sample.sh
+++ scripts/elixir-sample.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 date
-time ./scripts/_run.sh samples/sample-elixir
+time ./scripts/_run.sh testdata/sample-elixir
 date
 open -a "Google Chrome" generated/elixir/reports/index.html
--- scripts/kotlin-sample.sh
+++ scripts/kotlin-sample.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 date
-time ./scripts/_run.sh samples/sample-kotlin
+time ./scripts/_run.sh testdata/sample-kotlin
 date
 open -a "Google Chrome" generated/kotlin/reports/index.html
